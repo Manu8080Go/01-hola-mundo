46c61eecc015ae863f171def4f2ead136f7d6ba0
Manu8080Go <manugou8080@gmail.comm>
2023-05-30 15:05:16 -0600

    Agregar ejemplos de ngIf y ngFor en body component

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
 })
 
 export class BodyComponent {
+  frase: any = {
+    mensaje: 'Un gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker'
+  }
+
+  mostrar: boolean;
+  personajes: string[];
+
+  constructor() {
+    this.mostrar = true;
+    this.personajes = ['Spiderman', 'Venom', 'Dr. Octopus'];
+  }
+
+  MostrarOcultar(): boolean{
+    if(this.mostrar){
+      this.mostrar = false;
+    } else{
+      this.mostrar = true;
+    }
+
+    return this.mostrar;
+  }
 
 }
+
